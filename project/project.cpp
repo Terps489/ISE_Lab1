dd6730b78099f95377d43af751596c98618e45ef
eviaenoc <eviaenoc@student.21-school.ru~>
2025-09-18 18:30:24 +0300

    CMake auto-commit: test_file build

--- project/project.cpp
+++ project/project.cpp
@@ -8,6 +8,7 @@ extern "C" {
 }
 
 struct conf {
+    bool use_dir;
     std::string dir_start;
     std::string filenames;
     std::string dir_end;
@@ -18,7 +19,8 @@ int handler(void* user, const char* section, const char* name, const char* value
     std::string key = name;
     std::string val = value;
 
-    if (key == "dir_start") pconfig->dir_start = val;
+    if (key == "use_dir") pconfig -> use_dir = (val == "true");
+    else if (key == "dir_start") pconfig->dir_start = val;
     else if (key == "filename_start") pconfig->filenames = val;
     else if (key == "dir_end") pconfig->dir_end = val;
     else return 0;
@@ -35,6 +37,7 @@ int main() {
     // std::cout << "dir_start: " << cfg.dir_start << '\n';
     // std::cout << "filename:  " << cfg.filenames << '\n';
     // std::cout << "dir_end:   " << cfg.dir_end << '\n';
+    // std::cout << "use_dir: " << cfg.use_dir << '\n';
     if (cfg.filenames.empty()) {
         std::cerr << "В конфиге нет файлов.\n";
         return 1;
