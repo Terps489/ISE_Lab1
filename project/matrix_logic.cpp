0f03b1fdbb2af384e6804653a930af1414f10e4f
Timohaya <fitil230903@gmail.com>
2025-09-20 21:38:28 +0300

    Update matrix_logic.cpp

--- project/matrix_logic.cpp
+++ project/matrix_logic.cpp
@@ -26,37 +26,99 @@ void logic_start(std::string file_start) {
   Wanna_work_with_matrix(matrix);
 }
 
+
+void matini(int q[], int l)
+{
+    for (int i = 0; i < l; i++)
+    {
+        q[i] = 0;
+    }
+}
+
+int max(int i1, int i2)
+{
+    if (i1 > i2)
+    {
+        return i1;
+    }
+    return i2;
+}
+int max(int i1, int i2, int i3)
+{
+    if (max(i1, i2) < i3)
+    {
+        return i3;
+    }
+    return max(i1, i2);
+}
+int max(int i1, int i2, int i3, int i4)
+{
+    if (max(i1, i2) < max(i3, i4))
+    {
+        return max(i3, i4);
+    }
+    return max(i1, i2);
+}
+int max(int i1, int i2, int i3, int i4, int i5)
+{
+    if (max(i1, i2, i5) < max(i3, i4))
+    {
+        return max(i3, i4);
+    }
+    return max(i1, i2, i5);
+}
+int min(int q[10])
+{
+    int rez = 0;
+    for (int i = 1; i < 10; i++)
+    {
+        rez = q[rez] > q[i] ? q[i] : rez;
+    }
+    return rez;
+
+}
+int min(int q[], int porog)
+{
+    int rez = 0;
+    for (int i = 1; i < 10; i++)
+    {
+        rez = ((q[rez] > q[i]) && (q[i] > porog) || (q[rez] <= porog)) ? q[i] : rez;
+    }
+    return rez;
+}
+
 void Wanna_work_with_matrix(std::vector<std::vector<int>>& matrix){
-//   for (auto& row : matrix) {
-//     for (auto val : row) std::cout << val << " ";
-//     std::cout << "\n";
-//   }
-//   std::cout << std::endl;
     int s[3][10];
     for (int i = 0; i < 3; i++)
-      for (int j = 0; j < 10; j++)
-       s[i][j] = matrix[i][j];
+        for (int j = 0; j < 10; j++)
+            s[i][j] = matrix[j][i];
+
+    //vivod(s);
     int porog = 0;
     int r[30]; // коэффициенты сложности получения
     int por[10];// коэффициент операции
     int sch[30]; // коэффициенты для используемых значений
     int srt[10]; // упоряточенный массив
-    int cur;
 
     int tic[120];
-    int kolvoproc=10;
-
+    int kolvoproc = 10;
+    matini(r, 30);
+    matini(por, 10);
+    matini(sch, 30);
+    matini(srt, 10);
+    matini(tic, 120);
+    //matini(r, 30);
 
     for (int y = 0; y < 10; y++)
     {
         if (s[1][y] == -1)
         {
             porog += 10;
-            por[y] = porog+1;
+            por[y] = porog;
         }
         else
         {
-            por[y] = max(porog, r[s[1][y]], r[s[0][y]])+1;
+            por[y] = max(porog, r[s[1][y]], r[s[0][y]]) + 1;
             if (tic[por[y]] < kolvoproc)
             {
                 tic[por[y]]++;
@@ -81,11 +143,21 @@ void Wanna_work_with_matrix(std::vector<std::vector<int>>& matrix){
             }
             r[s[2][y]] = por[y];
 
-           
+
         }
     }
-    int beg=0;
-    int lvl= por[min(por)];
+
+    /*for (int i = 0; i < 10; i++)
+    {
+        std::cout << por[i] <<"----"<<i<< '\n';
+    }*/
+    int beg = 0;
+    int lvl = por[0];
+    int buf = 0;
+    for (int i = 1; i < 10; i++)
+    {
+        lvl = por[lvl] > por[i] ? por[i] : lvl;
+    }
     bool serch = true;
 
     for (int i = 0; i < 10; i++)
@@ -105,74 +177,31 @@ void Wanna_work_with_matrix(std::vector<std::vector<int>>& matrix){
             else
             {
                 beg = 0;
-                lvl = por[min(por, lvl)];
+                buf = lvl + 1;
+                for (int i = 1; i < 10; i++)
+                {
+                    lvl = ((lvl > por[i]) && (por[i] >= buf) || (lvl <= buf)) ? por[i] : lvl;
+                }
+                //lvl = por[min(por, lvl+1)];
 
             }
 
         }
         serch = true;
     }
-    vivod(s, srt);
-}
-int max(int i1, int i2)
-{
-    if (i1 > i2)
-    {
-        return i1;
-    }
-    return i2;
-}
-int max(int i1, int i2, int i3)
-{
-    if (max(i1, i2) > i3)
-    {
-        return i3;
-    }
-    return max(i1, i2);
-}
-int max(int i1, int i2, int i3, int i4)
-{
-    if (max(i1, i2) > max(i3,i4))
-    {
-        return max(i3, i4);
-    }
-    return max(i1, i2);
-}
-int max(int i1, int i2, int i3, int i4, int i5)
-{
-    if (max(i1, i2,i5) > max(i3, i4))
-    {
-        return max(i3, i4);
-    }
-    return max(i1, i2, i5);
-}
-int min(int q[10])
-{
-    int rez=0;
-    for (int i = 1; i < 10; i++)
-    {
-        rez = q[rez] > q[i] ? q[i] : rez;
-    }
-    return rez;
+    //vivod(s, srt,por);
 
-}
-int min(int q[10], int porog)
-{
-    int rez=0;
-    for (int i = 1; i < 10; i++)
-    {
-        rez = ((q[rez] > q[i]) &&(q[i]>porog) || (q[rez]<=porog)) ? q[i] : rez;
-    }
-    return rez;
-}
-
-
-void vivod(int s[3][10], int srt[10])
-{
     int cur;
     for (int i = 0; i < 10; i++)
     {
         cur = srt[i];
-        std::cout << "R" << s[0][cur] << " + R" << s[1][cur] << " --> " << s[2][cur];
+        std::cout << "R" << s[0][cur] << " + R" << s[1][cur] << " --> R" << s[2][cur] << "   Diff:" << por[cur] << "   Num:" << srt[i] << '\n';
     }
+    std::cout << '\n' << '\n';
+    cur = 0;
+    for (int i = 0; i < 120; i++)
+    {
+        cur = cur < tic[i] ? tic[i] : cur;
+    }
+    std::cout << cur;
 }
